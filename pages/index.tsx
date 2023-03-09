13d693ab5bb5193aef91a794b2af63f538850ec1
ilya smagin <ilya-z1999@ya.ru>
2023-03-10 00:15:17 +0300

    v0.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { useState } from 'react'
 import useSWR from "swr"
 import { ReactImageMosaic } from 'react-image-mosaic'
+import { blob } from 'stream/consumers';
 
 
 const later = (url:string) =>
@@ -5643,18 +5644,20 @@ const later = (url:string) =>
 const fakefetcher = (url: string) => later(url).then((r) => r.tracks.items.map(track => track.track.album.images[0].url));export default function Home() {
   //const [covers, setCovers] = useState(["https://i.scdn.co/image/ab67616d0000b27367c738a703dc979f5c3c52ef"]);
   const [playlistLink, setPlaylistLink] = useState("");
+  const [targetLink, setTargetLink] = useState();
   const playlistREGEX = /.*playlist\//i;
   const playlistURL = "https://spotify23.p.rapidapi.com/playlist_tracks/?id=" + playlistLink;
   const options = {
 	  method: 'GET',
 	  headers: {
-      'X-RapidAPI-Key': process.env.API_KEY,
-      'X-RapidAPI-Host': process.env.API_HOST
+      'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY,
+      'X-RapidAPI-Host': process.env.NEXT_PUBLIC_API_HOST
 	  }
   };
-  const fetcher = (url: string, ...args ) => fetch(url, ...args).then(res => { console.log("returned"); return res.json()}).then(res => res.tracks.items.map(track => track.track.album.images[0].url));
+  const fetcher = (url: string, ...args) => fetch(url, ...args).then(res => res.json()).then(res => { return res.tracks.items.map(track => track.track.album.images[0].url) });
 
-  const { data, error, isLoading } = useSWR(playlistLink.length>1 ? [playlistURL, options] : null, ([playlistURL, options])=>fetcher(playlistURL, options));
+  const { data, error, isLoading } = useSWR(playlistLink.length > 1 ? [playlistURL, options] : null, ([playlistURL, options]) => fetcher(playlistURL, options));
+
   return (
     <>
       <Head>
@@ -5668,16 +5671,15 @@ const fakefetcher = (url: string) => later(url).then((r) => r.tracks.items.map(t
         <input id="playlistInput" onChange={(e)=>setPlaylistLink(e.target.value.replace(playlistREGEX, "")) } className="focus:placeholder:text-green-400 placeholder:text-gray-400 border-b-2 focus:border-green-400 border-gray-400 appearance-none w-2/3 text-3xl bg-none px-8 py-4 shadow-sm focus:outline-none focus:caret-green-500 text-green-400 bg-black dark:highlight-white/5" placeholder='playlist link'/>
         <label htmlFor='targetImageInput' className='mt-12 text-3xl text-gray-300 w-2/3 mb-2'>Link or upload image</label>
         <div className='w-2/3 flex flex-row'>
-          <input id='targetImageInput' className="focus:placeholder:text-green-400 placeholder:text-gray-400 border-b-2 focus:border-green-400 border-gray-400 appearance-none w-2/3 text-3xl bg-none px-8 shadow-sm focus:outline-none focus:caret-green-500 text-gray-400 bg-black dark:highlight-white/5" placeholder='image link' />
+          <input id='targetImageInput'  onChange={(e)=>setTargetLink(e.target.value) } className="focus:placeholder:text-green-400 placeholder:text-gray-400 border-b-2 focus:border-green-400 border-gray-400 appearance-none w-2/3 text-3xl bg-none px-8 shadow-sm focus:outline-none focus:caret-green-500 text-gray-400 bg-black dark:highlight-white/5" placeholder='image link' />
           <label className="w-1/3 flex justify-center items-end">
             <span className="sr-only">Choose profile photo</span>
-            <input type="file" className="block w-full text-sm pl-4 text-gray-500 file:flex text-center file:w-full file:flex-col file:mb-2 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-gray-100 file:text-green-600 hover:file:bg-green-200 " />
+            <input type="file" onChange={(e)=> setTargetLink(e.target.files[0])} className="block w-full text-sm pl-4 text-gray-500 file:flex text-center file:w-full file:flex-col file:mb-2 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-gray-100 file:text-green-600 hover:file:bg-green-200 " />
           </label>
         </div>
         <div className='w-2/3 aspect-square flex justify-center items-center mt-12 collage-wrapper mb-12'>{/*aito-cols-[100%/n]*/}
-          {data ?
-            <ReactImageMosaic width={800} height={800} rows={Math.round(Math.sqrt(data.length))*2} columns={Math.round(Math.sqrt(data.length))*2} sources={data} colorBlending={0.15} target={"./mal.jpg"} crossOrigin={"anonymous"} />
-            : <div className='text-gray-300 animate-pulse'>LOADING</div>}
+          {data ? <ReactImageMosaic width={800} height={800} rows={40} columns={40} sources={data} colorBlending={0.15} target={targetLink ? URL.createObjectURL(targetLink) : "./mal.jpg"} crossOrigin={"anonymous"} /> : null}
+          {isLoading ? <div className='text-gray-300 animate-pulse'>Loading your playlist...</div>:null}
         </div>
       </main>
     </>
