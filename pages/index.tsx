675b90d2dfaa6256236de1b43d161ffdd5ab1790
ilya smagin <ilya-z1999@ya.ru>
2023-04-13 20:36:14 +0300

    overlay buttons and state-based rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import useSWR from "swr";
 import { ReactImageMosaic } from "react-image-mosaic";
 import { blob } from "stream/consumers";
@@ -8,6 +8,7 @@ export default function Home() {
   const [playlistLink, setPlaylistLink] = useState("");
   const [startGeneration, setStartGeneration] = useState(false);
   const [targetLink, setTargetLink] = useState<string>();
+  const [showCanvasMenu, setShowCanvasMenu] = useState(false);
   const playlistREGEX = /.*playlist\//i;
   const playlistURL =
     "https://spotify23.p.rapidapi.com/playlist_tracks/?id=" + playlistLink;
@@ -36,6 +37,24 @@ export default function Home() {
     playlistLink.length > 1 ? [playlistURL, options] : null,
     ([playlistURL, options]) => fetcher(playlistURL)
   );
+  function downloadURI(uri: string, name: string) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  const handleExport = () => {
+    const canvas = document.getElementsByTagName(
+      "canvas"
+    )[0] as HTMLCanvasElement;
+    const uri = canvas.toDataURL();
+    downloadURI(uri, "collage.png");
+  };
+  const generateCollage = () => {
+    if (data && targetLink) setStartGeneration((prev) => !prev);
+  };
 
   return (
     <>
@@ -49,8 +68,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="mx-3 mt-5 text-white">
-        <h1 className="my-2 text-3xl font-bold">Playlist collage</h1>
+      <header className="mx-3 mt-3 text-white">
+        <h1 className=" my-1 text-2xl font-bold">Playlist collage</h1>
         <nav className="gap-7 flex flex-row mx-3 text-xl">
           <button className="border-b border-green-500 appearance-none">
             Albums
@@ -61,42 +80,73 @@ export default function Home() {
         </nav>
       </header>
 
-      <main className="flex flex-col items-center justify-center mx-6 mt-4">
-        <div className="relative sm:w-2/3 md:w-1/3 aspect-square md:flex-row bg-zinc-800 flex flex-col items-center justify-center w-full">
-          <div className="aspect-square collage-wrapper flex items-center justify-center w-full mt-12 mb-12 absolute pointer-events-none">
+      <main className="flex flex-col items-center justify-center mx-6 mt-3">
+        <div
+          className={
+            "sm:w-2/3 md:w-1/3 aspect-square bg-zinc-800 relative flex flex-col items-center justify-center w-full"
+          }
+        >
+          {showCanvasMenu ? (
+            <div
+              className="aspect-square collage-wrapper bg-zinc-800/50 absolute z-20 flex flex-col items-center justify-center w-full mt-12 mb-12 text-white"
+              onClick={() => setShowCanvasMenu((prev) => !prev)}
+            >
+              <button
+                className=" active:scale-95 px-10 py-3 mt-5 mb-3 text-sm font-bold text-white transition-transform bg-green-500 rounded-full"
+                onClick={handleExport}
+              >
+                Download
+              </button>
+              or
+              <label
+                className=" flex flex-col items-end justify-center pt-2"
+                htmlFor="photoUpload"
+              >
+                <button className="active:scale-95 px-10 py-3 mt-5 mb-3 text-sm font-bold text-white transition-transform bg-transparent border-[1px] rounded-full">
+                  Upload new image
+                </button>
+              </label>
+            </div>
+          ) : null}
+          <div className="aspect-square collage-wrapper absolute flex flex-col items-center justify-center w-full mt-12 mb-12 z-10">
             {data && startGeneration ? (
-              <ReactImageMosaic
-                width={800}
-                height={800}
-                rows={40}
-                columns={40}
-                sources={data}
-                colorBlending={0.15}
-                target={targetLink || "./mal.jpg"}
-                crossOrigin={"anonymous"}
-              />
-            ) : null}
+              <div onClick={() => setShowCanvasMenu((prev) => !prev)}>
+                <ReactImageMosaic
+                  width={800}
+                  height={800}
+                  rows={40}
+                  columns={40}
+                  sources={data}
+                  colorBlending={0.15}
+                  target={targetLink || "./mal.jpg"}
+                  crossOrigin={"anonymous"}
+                />
+              </div>
+            ) : (
+              <label
+                className=" flex flex-col items-end justify-center pt-2"
+                htmlFor="photoUpload"
+              >
+                <span className="sr-only">Upload target photo</span>
+                <input
+                  id="photoUpload"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files instanceof FileList)
+                      return setTargetLink(
+                        URL.createObjectURL(e.target.files[0])
+                      );
+                  }}
+                  className=" file:flex file:w-full file:flex-col file:mb-2 file:py-1 file:px-4 file:rounded-full file:border-[1px] file:border-zinc-500 file:text-sm file:font-semibold file:bg-transparent file:appearance-none file:text-gray-400 hover:file:bg-zinc-200 hover:file:text-slate-900 block text-sm text-center text-gray-500"
+                />
+              </label>
+            )}
             {isLoading ? (
               <div className="animate-pulse text-gray-300">
                 Loading your playlist...
               </div>
             ) : null}
           </div>
-          <label
-            className=" flex flex-col items-end justify-center pt-2"
-            htmlFor="photoUpload"
-          >
-            <span className="sr-only">Upload target photo</span>
-            <input
-              id="photoUpload"
-              type="file"
-              onChange={(e) => {
-                if (e.target.files instanceof FileList)
-                  return setTargetLink(URL.createObjectURL(e.target.files[0]));
-              }}
-              className=" file:flex file:w-full file:flex-col file:mb-2 file:py-1 file:px-4 file:rounded-full file:border-[1px] file:border-zinc-500 file:text-sm file:font-semibold file:bg-transparent file:appearance-none file:text-gray-400 hover:file:bg-zinc-200 hover:file:text-slate-900 block text-sm text-center text-gray-500"
-            />
-          </label>
         </div>
         <div className="text-while w-full mx-6 my-3">
           <label
@@ -110,12 +160,12 @@ export default function Home() {
             onChange={(e) =>
               setPlaylistLink(e.target.value.replace(playlistREGEX, ""))
             }
-            className="focus:placeholder:text-green-400 mt-1 placeholder:text-gray-400 focus:border-green-400 bg-none focus:outline-none focus:caret-green-500 dark:highlight-white/5 w-full px-8 py-2 text-sm text-green-400 bg-zinc-800 rounded-md appearance-none"
+            className="focus:placeholder:text-green-400 placeholder:text-gray-400 focus:border-green-400 bg-none focus:outline-none focus:caret-green-500 dark:highlight-white/5 bg-zinc-800 w-full px-8 py-2 mt-1 text-sm text-green-400 rounded-md appearance-none"
             placeholder="Link to playlist"
           />
         </div>
-        <form className="flex flex-col items-center justify-center mx-6 text-white w-full">
-          <div className="flex flex-row items-center justify-center gap-3 my-1 text-sm md:text-base">
+        <form className="flex flex-col items-center justify-center w-full mx-6 text-white">
+          <div className="md:text-base flex flex-row items-center justify-center gap-3 my-1 text-sm">
             <input
               type="radio"
               name="collageOptions"
@@ -126,7 +176,7 @@ export default function Home() {
               htmlFor="size"
               className="px-3 py-1 font-semibold border-[1px] border-zinc-700 rounded-full appearance-none"
             >
-              <span className="hidden sm:block">Image size</span>{" "}
+              <span className="sm:block hidden">Image size</span>{" "}
               <span className="sm:hidden">Size</span>
             </label>
             <input
@@ -151,7 +201,7 @@ export default function Home() {
               htmlFor="grid"
               className="px-3 py-1 font-semibold border-[1px] border-zinc-700 rounded-full appearance-none"
             >
-              <span className="hidden sm:block">Grid size</span>{" "}
+              <span className="sm:block hidden">Grid size</span>{" "}
               <span className="sm:hidden">Grid</span>
             </label>
           </div>
@@ -162,12 +212,17 @@ export default function Home() {
             min="4"
             max="40"
             step="1"
-            className="appearance-none bg-black color-gray-500 w-full"
+            className="appearance-none bg-transparent color-gray-500 w-full mt-1.5"
           />
         </form>
         <button
-          className=" bg-green-500 text-white font-bold rounded-full px-10 py-3 text-sm mt-5 mb-3 active:scale-95 transition-transform"
-          onClick={() => setStartGeneration((prev) => !prev)}
+          className={
+            " active:scale-95 px-10 py-3 mt-5 mb-3 text-sm font-bold  transition-transform " +
+            (data && targetLink
+              ? "bg-green-500 rounded-full text-white"
+              : "bg-transparent border-[1px] border-zinc-400 rounded-full text-zinc-400")
+          }
+          onClick={generateCollage}
         >
           Generate Collage
         </button>
